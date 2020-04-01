136d90a7313b340b2e794f002c7e7d7146bbec29
Brianna <bs3864@nyu.edu>
2020-04-01 01:40:05 -0400

    Picking up Pictures

---
+++ HappyThings/Assets/Scripts/DresserPictures.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DresserPictures : MonoBehaviour
+{
+    public Camera camera;
+    public GameObject[] familyPhotos;
+
+    public Vector3[] photoStartingLocation; 
+
+    private bool selectionMode;
+    private int selectedPhoto; 
+    
+    void Start()
+    {
+        selectionMode = true; 
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (selectionMode)
+                {
+                    if (hit.transform.name == "WeddingPhoto")
+                    {
+                        print("My object is clicked by mouse");
+                        selectedPhoto = 1;
+                        SelectedPhoto(1);
+                    }
+
+                    if (hit.transform.name == "CarPhoto")
+                    {
+                        selectedPhoto = 0;
+                        SelectedPhoto(0);
+                    }
+                    if (hit.transform.name == "BabyPhoto")
+                    {
+                        selectedPhoto = 2; 
+                        SelectedPhoto(2);
+                    }
+                    if (hit.transform.name == "SiblingPhoto")
+                    {
+                        selectedPhoto = 3; 
+                        SelectedPhoto(3);
+                    }
+                    if (hit.transform.name == "XmasPhoto")
+                    {
+                        selectedPhoto = 4; 
+                        SelectedPhoto(4);
+                    }
+                }
+                else if(!selectionMode)
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        ReturnPhoto(selectedPhoto);
+                    }
+                    
+                }
+            }
+        }
+    }
+
+  
+    
+    private void SelectedPhoto(int photoNumber)
+    {
+        selectionMode = false;
+        familyPhotos[photoNumber].transform.localPosition = new Vector3(-2.0f, 1.5f, -2.5f);
+    }
+
+    private void ReturnPhoto(int photoNumber)
+    {
+        familyPhotos[photoNumber].transform.localPosition = photoStartingLocation[photoNumber];
+        selectionMode = true;
+    }
+}
---
+++ HappyThings/Assets/Scripts/TVArea.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus; 
+
+public class TVArea : MonoBehaviour
+{
+    public Camera camera;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name == "TVHotspot")
+                {
+                    Fungus.Flowchart.BroadcastFungusMessage("TVON");
+                }
+            }
+        }
+    }
+}
